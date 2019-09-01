d6a2de5584b734ad86b6071a18959d4078798e7e
Jorge Almagro <jorge.almagro@a3gconsultores.com>
2019-09-01 23:39:02 +0200

    Initial version

---
+++ src/main/java/es/almagrofernandez/blog_medio/Medio.java
@@ -0,0 +1,11 @@
+package es.almagrofernandez.blog_medio;
+
+import es.almagrofernandez.blog_base.Base;
+
+public class Medio {
+	Base base = new Base();
+
+	public Medio() {
+		System.out.println("Creado objeto Medio");
+	}
+}
